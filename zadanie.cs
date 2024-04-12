e2412ee8f4b1e9a18f10be34482a31f4c31e9059
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-04-12 13:52:00 +0700

    Update zadanie.cs

--- zadanie.cs
+++ zadanie.cs
@@ -1,17 +1,37 @@
-using System;
-using System.Text.RegularExpressions;
+﻿using System;
 
 class Program
 {
     static void Main()
     {
-        string stroka = "11001101000110";
-        Regex regular = new Regex("1[0]+1");
-        MatchCollection matches = regular.Matches(stroka);
-        
-        foreach (Match match in matches)
+        string str = "sd10sds101sdd100101"; // строка
+        int inState = 0;
+        string output = "";
+
+        for (int i = 0; i < str.Length; i++)
         {
-            Console.WriteLine(match.Value);
+            if (str[i] == '1' && inState == 0)
+            {
+                inState = 1;
+                output = "1";
+                continue;
+            }
+            if (str[i] == '1' && inState == 2)
+            {
+                output += "1";
+                Console.WriteLine(output);
+                inState = 0;
+                i--;
+            }
+            if (str[i] == '0' && (inState == 1 || inState == 2))
+            {
+                output += "0";
+                inState = 2;
+            }
+            else if (inState == 1 || inState == 2)
+            {
+                inState = 0;
+            }
         }
     }
 }
