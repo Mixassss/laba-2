f8f6d14ab9f91ebda24dac2cb1ea5dbef16e3531
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-04-12 13:56:52 +0700

    Update z.py

--- z.py
+++ z.py
@@ -1,8 +1,19 @@
-import re
-
-stroka = "11001101000110"
-regular = "1[0]+1"
-res = re.finditer(regular, stroka)
-
-for match in res:
-    print(match.group(0))
+str = "sd10sds101sdd100101"  # строка
+in_ = 0
+out = ""
+i = 0
+while i < len(str):
+    if str[i] == '1' and in_ == 0:
+        in_ = 1
+        out = '1'
+    elif str[i] == '1' and in_ == 2:
+        out += '1'
+        print(out)
+        in_ = 0
+        i -= 1
+    elif str[i] == '0' and (in_ == 1 or in_ == 2):
+        out += '0'
+        in_ = 2
+    elif in_ == 1 or in_ == 2:
+        in_ = 0
+    i += 1
