35062a0c87ce1c3488eea098a1974af18b430273
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-04-12 13:57:53 +0700

    Update hyt.swift

--- hyt.swift
+++ hyt.swift
@@ -1,13 +1,29 @@
 import Foundation
 
-let stroka = "11001101000110"
-let regular = try! NSRegularExpression(pattern: "1[0]+1")
-let range = NSRange(location: 0, length: stroka.utf16.count)
+var str = "sd10sds101sdd100101"
+var inVar = 0
+var out = ""
 
-var startIndex = 0
-while let match = regular.firstMatch(in: stroka, options: [], range: NSRange(startIndex..<stroka.utf16.count)) {
-    if let swiftRange = Range(match.range, in: stroka) {
-        print(String(stroka[swiftRange]))
-        startIndex = match.range.upperBound
+for i in 0..<str.count {
+    let char = str[str.index(str.startIndex, offsetBy: i)]
+    
+    if char == "1" && inVar == 0 {
+        inVar = 1
+        out = "1"
+        continue
+    }
+    
+    if char == "1" && inVar == 2 {
+        out += "1"
+        print(out)
+        inVar = 0
+        i -= 1
+    }
+    
+    if char == "0" && (inVar == 1 || inVar == 2) {
+        out += "0"
+        inVar = 2
+    } else if inVar == 1 || inVar == 2 {
+        inVar = 0
     }
 }
