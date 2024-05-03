e6cc5a0e5b2b93383dded9e5cbc4cf1f26363b33
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-05-04 01:00:17 +0700

    Update third_zadanie.js

--- third_zadanie.js
+++ third_zadanie.js
@@ -1,33 +1,30 @@
 function sumOfDigits(n) {
-    let sum = 0;
-    let numStr = n.toString();
-    for (let i = 0; i < numStr.length; i++) {
-      sum += parseInt(numStr.charAt(i));
-    }
-    return sum;
+  let sum = 0;
+  while (n > 0) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
   }
-  
-  function productOfDigits(n) {
-    let result = 1;
-    let numStr = n.toString();
-    for (let i = 0; i < numStr.length; i++) {
-      result *= parseInt(numStr.charAt(i));
-    }
-    return result;
+  return sum;
+}
+
+function productOfDigits(n) {
+  let product = 1;
+  while (n > 0) {
+      product *= n % 10;
+      n = Math.floor(n / 10);
   }
-  
-  function findNumbers(sequence) {
-    let result = [];
-    for (let i = 0; i < sequence.length; i++) {
-      if (sequence[i] >= 0 && sumOfDigits(sequence[i]) < productOfDigits(sequence[i])) {
-        result.push(i + 1);
-      }
-    }
-    return result;
+  return product;
+}
+
+function checkNumber(n) {
+  return sumOfDigits(n) < productOfDigits(n);
+}
+
+const numbers = [3, 27, 14, 99];
+let index = 1;
+for (let number of numbers) {
+  if (checkNumber(number)) {
+      process.stdout.write(index + ' ');
   }
-  
-  let sequence = [3, 27, 14, 99];
-  let output = findNumbers(sequence);
-  output.forEach(num => {
-    console.log(num + " ");
-  });
+  index++;
+}
