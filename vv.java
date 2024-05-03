79342658dc67a11af3ddbb2408590e286d0e6284
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-05-04 01:01:56 +0700

    Update vv.java

--- vv.java
+++ vv.java
@@ -1,41 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class vv {
-    
     public static void main(String[] args) {
-        int[] sequence = {3, 27, 14, 99};
-        int[] output = findNumbers(sequence);
-        for (int num : output) {
-            System.out.print(num + " ");
-        }
-    }
-
-    public static int sumOfDigits(int n) {
-        int sum = 0;
-        String numStr = String.valueOf(n);
-        for (int i = 0; i < numStr.length(); i++) {
-            sum += Character.getNumericValue(numStr.charAt(i));
-        }
-        return sum;
-    }
-
-    public static int productOfDigits(int n) {
-        int result = 1;
-        String numStr = String.valueOf(n);
-        for (int i = 0; i < numStr.length(); i++) {
-            result *= Character.getNumericValue(numStr.charAt(i));
-        }
-        return result;
-    }
-
-    public static int[] findNumbers(int[] sequence) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < sequence.length; i++) {
-            if (sequence[i] >= 0 && sumOfDigits(sequence[i]) < productOfDigits(sequence[i])) {
-                result.add(i + 1);
+        int[] числа = {3, 27, 14, 99};
+        int номер = 1;
+        for (int число : числа) {
+            if (проверка_числа(число)) {
+                System.out.print(номер + " ");
             }
+            номер++;
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int сумма_цифр(int n) {
+        int сумма = 0;
+        while (n > 0) {
+            сумма += n % 10;
+            n /= 10;
+        }
+        return сумма;
+    }
+
+    public static int произведение_цифр(int n) {
+        int произведение = 1;
+        while (n > 0) {
+            произведение *= n % 10;
+            n /= 10;
+        }
+        return произведение;
+    }
+
+    public static boolean проверка_числа(int n) {
+        return сумма_цифр(n) < произведение_цифр(n);
     }
 }
