20aeff7129e6f77b16a9140d5dd08d4035e643a9
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-05-04 01:03:25 +0700

    Update 3z.cs

--- 3z.cs
+++ 3z.cs
@@ -1,42 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
     static int SumOfDigits(int n)
     {
-        return n.ToString().Sum(c => c - '0');
+        int sum = 0;
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
-
     static int ProductOfDigits(int n)
     {
-        int result = 1;
-        foreach (char digit in n.ToString())
+        int product = 1;
+        while (n > 0)
         {
-            result *= digit - '0';
+            product *= n % 10;
+            n /= 10;
         }
-        return result;
+        return product;
     }
-
-    static List<int> FindNumbers(List<int> sequence)
+    static bool CheckNumber(int n)
     {
-        List<int> result = new List<int>();
-        for (int i = 0; i < sequence.Count; i++)
-        {
-            int number = sequence[i];
-            if (number >= 0 && SumOfDigits(number) < ProductOfDigits(number))
-            {
-                result.Add(i + 1);
-            }
-        }
-        return result;
+        return SumOfDigits(n) < ProductOfDigits(n);
     }
 
     static void Main()
     {
-        List<int> sequence = new List<int> { 3, 27, 14, 99 };
-        List<int> output = FindNumbers(sequence);
-        Console.WriteLine(string.Join(", ", output));
+        int[] numbers = { 3, 27, 14, 99 };
+        int index = 1;
+        foreach (int number in numbers)
+        {
+            if (CheckNumber(number))
+            {
+                Console.Write(index + " ");
+            }
+            index++;
+        }
     }
 }
