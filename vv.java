e3797478216f2661f6b2a4fe1dc3ae3a90dd53da
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-05-11 15:14:57 +0700

    Update vv.java

--- vv.java
+++ vv.java
@@ -1,34 +1,43 @@
 public class vv {
+    public static int sumDigits(int n) {
+        int s = 0;
+        while (n != 0) {
+            s += n % 10;
+            n /= 10;
+        }
+        return s;
+    }
+
+    public static int productDigits(int n) {
+        int p = 1;
+        while (n != 0) {
+            p *= n % 10;
+            n /= 10;
+        }
+        return p;
+    }
+
+    public static boolean checkNum(int n) {
+        return sumDigits(n) < productDigits(n);
+    }
+
     public static void main(String[] args) {
-        int[] числа = {3, 27, 14, 99};
-        int номер = 1;
-        for (int число : числа) {
-            if (проверка_числа(число)) {
-                System.out.print(номер + " ");
+        Scanner scanner = new Scanner(System.in);
+        String[] numbersStr = scanner.nextLine().split(" ");
+        List<Integer> numbers = new ArrayList<>();
+        for (String numStr : numbersStr) {
+            numbers.add(Integer.parseInt(numStr));
+        }
+
+        List<String> result = new ArrayList<>();
+        int index = 1;
+        for (int number : numbers) {
+            if (checkNum(number)) {
+                result.add(String.valueOf(index));
             }
-            номер++;
+            index++;
         }
-    }
 
-    public static int сумма_цифр(int n) {
-        int сумма = 0;
-        while (n > 0) {
-            сумма += n % 10;
-            n /= 10;
-        }
-        return сумма;
-    }
-
-    public static int произведение_цифр(int n) {
-        int произведение = 1;
-        while (n > 0) {
-            произведение *= n % 10;
-            n /= 10;
-        }
-        return произведение;
-    }
-
-    public static boolean проверка_числа(int n) {
-        return сумма_цифр(n) < произведение_цифр(n);
+        System.out.println(String.join(" ", result));
     }
 }
